1c549acf9f73348bda2ab8953d8ababe80fea9fa
weversonisaquell-sys <weversonisaquell@gmail.com>
2026-08-13 22:42:49 -0300

    Add Material Components library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.compose.material:material-icons-core")
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.core:core-ktx:1.13.1")
